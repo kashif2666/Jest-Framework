14c146c6cf8fea3bdc519dd629f1325da7d93b7b
kashif2666 <kashifjsr7@gmail.com>
2024-09-05 17:42:17 +0530

    Test Input Box

--- src/App.js
+++ src/App.js
@@ -5,6 +5,16 @@ function App() {
     <div className="App">
       <p>First React Test Case12</p>
       <p>Kashif Anwar</p>
+      <input
+        type="text"
+        placeholder="Enter username"
+        name="username"
+        id="userId"
+        value="Aman"
+        readOnly
+      />
+      <br />
+      <br /> <br /> <br />
       <img
         title="Man In Weed"
         src="https://plus.unsplash.com/premium_photo-1725024141611-52f6d3870816?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
--- src/App.test.js
+++ src/App.test.js
@@ -10,3 +10,15 @@ test("First React app case", () => {
   expect(text2).toBeInTheDocument();
   expect(title).toBeInTheDocument();
 });
+
+test("Testing Input Box", () => {
+  render(<App />);
+  const checkInput = screen.getByRole("textbox");
+  const checkInputPlaceholder = screen.getByPlaceholderText("Enter username");
+  expect(checkInput).toBeInTheDocument();
+  expect(checkInputPlaceholder).toBeInTheDocument();
+  expect(checkInput).toHaveAttribute("name", "username");
+  expect(checkInput).toHaveAttribute("id", "userId");
+  expect(checkInput).toHaveAttribute("type", "text");
+  expect(checkInput).toHaveAttribute("value", "Aman");
+});
